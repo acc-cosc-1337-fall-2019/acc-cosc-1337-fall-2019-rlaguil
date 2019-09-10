e788602df379bc65042d19a1597baa06b56b1e1c
8584013 <8584013@RRC2220-15>
2019-09-09 19:04:14 -0500

    classwork

--- src/examples/02_module/03_switch/switch.cpp
+++ src/examples/02_module/03_switch/switch.cpp
@@ -1,6 +1,7 @@
 //write include statement for switch header
 #include "switch.h"
 
+
 //Write code for function that accepts a num and returns a string 
 //as follows:
 //returns 'Option 1' when value 1
@@ -9,3 +10,21 @@
 //returns 'Option 4' when value 4
 //return 'Invalid Option' otherwise
 
+string menu(int menu_option)
+{
+	switch (menu_option)
+	{
+	case 1:
+		return "option 1";
+	case 2:
+		return "option 2";
+	case 3:
+		return "option 3";
+	case 4:
+		return "option 4";
+	default:
+		return "invalid";
+	}
+	
+	return string();
+}
