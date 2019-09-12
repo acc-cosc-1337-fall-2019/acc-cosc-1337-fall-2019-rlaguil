eca8f550b65e503e28ab3b81fe6b36ec1f87cef2
8584013 <8584013@RRC2220-15>
2019-09-11 19:13:38 -0500

    Romeo HWWW

--- test/examples_test/02_module_test/02_module_tests.cpp
+++ test/examples_test/02_module_test/02_module_tests.cpp
@@ -2,6 +2,7 @@
 #include "catch.hpp"
 #include "if.h"
 #include "if_else.h"
+#include "switch.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -21,4 +22,13 @@ TEST_CASE("Test get genreation")
 	REQUIRE(get_generation(1960) == "Baby Boomer");
 	REQUIRE(get_generation(1940) == "Silent Generation");
 	REQUIRE(get_generation(1776) == "Invalid");
+}
+
+TEST_CASE("Test menu function")
+{
+	REQUIRE(menu(1) == "option 1");
+	REQUIRE(menu(2) == "option 2");
+	REQUIRE(menu(3) == "option 3");
+	REQUIRE(menu(4) == "option 4");
+	REQUIRE(menu(100) == "invalid");
 }
