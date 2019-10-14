09635b84c7af814d06a5e8e3d4f54e147ed7ac73
8584013 <8584013@RRC2220-15>
2019-10-14 18:49:02 -0500

    before customer change

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -1,25 +1,32 @@
 #include "atm.h"
 #include <vector>
 #include <iostream>
+#include"customer.h"
 
 
-using std::cout; using std::vector;
+using std::cout; using std::vector; using std::cin;
 
 int main()
 {
-	BankAccount a;
+	/*BankAccount a;
 	BankAccount b = a;
 	//scan card and pin number
 	//get  customer record from db into a vector
 	vector<BankAccount>accounts{ BankAccount(500),BankAccount(500),BankAccount(600),
-	BankAccount(1000)};
+	BankAccount(1000)};*/
+	Customer customer;
+	customer.add_account(BankAccount(500));
+	customer.add_account(BankAccount(1500));
+	customer.add_account(BankAccount(5000));
 
-	BankAccount account(9000);
+	cout << customer;
+
+	/*BankAccount account(9000);
 	ATM atm(account);
 	atm.deposit(100);
 	atm.withdraw(50);
 	atm.display_balance();
-	//cout<atm>
+	//cout<atm>*/
 
 	return 0;
 }
