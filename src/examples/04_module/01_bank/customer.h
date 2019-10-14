6f5acfaa876e0650dba3850d8366606b3e9c91f2
8584013 <8584013@RRC2220-15>
2019-10-14 18:15:37 -0500

    class work 10/14/19

--- src/examples/04_module/01_bank/customer.h
+++ src/examples/04_module/01_bank/customer.h
@@ -1,1 +1,18 @@
+#include<vector>
+#include<iostream>
+#include "bank_account.h"
 //customer.h
+
+using std::vector;
+using std::ostream;
+
+class Customer
+{
+public:
+	void add_account(BankAccount act);
+	friend ostream& operator<<(ostream& out, const Customer& act);
+
+private:
+	vector<BankAccount> accounts;
+	int total_balance{ 0 };
+};
