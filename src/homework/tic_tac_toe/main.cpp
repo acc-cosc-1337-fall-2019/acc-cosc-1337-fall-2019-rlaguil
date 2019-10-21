2fe671e53840c7b0cd478aad3ce42adccb094228
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2019-10-20 22:44:05 -0500

    Homework 6 help
    Homework 6 assistance to help students for this week's assignment

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,6 +1,38 @@
+#include"tic_tac_toe.h"
+#include<iostream>
+#include<string>
+
+using std::cout; using std::cin; using std::string;
 
 int main() 
 {
+	char cont = 'y';
+	TicTacToe game;
+	string player;
+	int position;
+
+	do 
+	{
+		cout << "Enter X or O";
+		cin >> player;
+
+		game.start_game(player);
+
+		while(game.game_over() == false)
+		{
+			cout << "Enter position from 1 to 9: ";
+			cin >> position;
+			game.mark_board(position);
+			game.display_board();
+		}
+	
+		cout << "Game over: ";
+
+		cout << "Play again y";
+		cin >> cont;
+
+	} while (cont == 'y' || cont == 'Y');
+
 
 	return 0;
 }
--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,28 @@
+#include<string>
+#include<vector>
+
+using std::string; using std::vector;
+
 //h
+/*
+Represents one game of tic tac toe.
+*/
+class TicTacToe 
+{
+public:
+	bool game_over();
+	void start_game(string player);
+	void mark_board(int position);
+	string get_player() const;
+	void display_board() const;
+
+private:
+	void set_next_player();
+	bool check_column_win();
+	bool check_row_win();
+	bool check_diagonal_win();
+	void clear_board();
+	bool check_board_full();
+	vector<string> pegs{ 9, " " };//initializer
+	string next_player;
+};
