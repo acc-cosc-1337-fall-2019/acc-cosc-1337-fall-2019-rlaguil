cfd54521958e2be61f1ebb41235043be03e1d7dd
8584013 <8584013@RRC2220-15>
2019-11-04 18:50:56 -0600

    panel, frame, main changes / start up

--- src/examples/08_module/frame.cpp
+++ src/examples/08_module/frame.cpp
@@ -1,1 +1,9 @@
 //frame.cpp
+#include "panel.h"
+#include "frame.h"
+Frame::Frame()
+	: wxFrame(NULL, wxID_ANY, "Hello World",
+		wxDefaultPosition, wxSize(1280, 760))
+{
+	auto panel = new Panel(this);
+}
