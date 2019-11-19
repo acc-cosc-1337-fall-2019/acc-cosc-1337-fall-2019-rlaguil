53ce9f371799fb3ca24b2dfa47a2e44c57e79010
8584013 <8584013@RRC2220-15>
2019-11-18 18:18:34 -0600

    pointers and refrences example 11

--- src/examples/11_module/01_ref_pointers/ref_pointers.cpp
+++ src/examples/11_module/01_ref_pointers/ref_pointers.cpp
@@ -1,9 +1,10 @@
 #include "ref_pointers.h"
 
-//
+// show that references and pointes can modify data pointed to
 
 void ref(int &number, int* numberPtr)
 {
-	
+	number = 500;
+	*numberPtr = 1000;
 }
 
